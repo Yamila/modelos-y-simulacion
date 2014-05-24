69392234d61c08ec99f3db4c46ba98c17ff19e26
Yamila <yamila@peque.(none)>
2014-05-24 19:13:55 -0300

    menu de snakepy

---
+++ tp2/lluvia/lluvia.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#! /usr/local/bin/python2.7
+import pygame, sys
+from pygame.locals import *
+from math import *
+from color import *
+from random import *
--- tp2/snakepy/snake.py
+++ tp2/snakepy/snake.py
@@ -6,6 +6,7 @@ from math import *
 from color import *
 from random import *
 from gusano import *
+from menu import *
 # Constantes
 ancho = 400
 alto = 400
@@ -17,34 +18,32 @@ angulo = 0
 # Distancia entre una parte del cuerpo y la siguiente
 distancia = 14  
 
-def main():
+
+def snake():
 	
-	def borrar(posicion):
-		pygame.draw.circle(fondo,negro,(posicion[0],posicion[1]),radio,grosor)
+	def borrar(posicion,color=negro):
+		pygame.draw.circle(fondo,color,(posicion[0],posicion[1]),radio,grosor)
 		pantalla.blit(fondo,(0,0))
 	
-	def dibujar(posicion):
-		pygame.draw.circle(fondo,blanco,(posicion[0],posicion[1]),radio,grosor)
+	def dibujar(posicion,color=blanco):
+		pygame.draw.circle(fondo,color,(posicion[0],posicion[1]),radio,grosor)
 		pantalla.blit(fondo,(0,0))
 
-	#crear ventana 
-	pantalla = pygame.display.set_mode((ancho,alto))
-	pygame.display.set_caption('SNAKE')
 	clock = pygame.time.Clock()
 	fondo = pygame.display.get_surface()
 	fondo = fondo.convert() #convertir la superficie al mismo formato de píxel que la pantalla
 	fondo.fill(negro)
 	# Crear Gusano
 	gusano = Gusano(ancho,alto,angulo,distancia,radio)
-	dibujar(gusano.cuerpo[gusano.cabeza])
+	dibujar(gusano.cuerpo[gusano.cabeza],verde_snake)
 	
 	while  gusano.tam <= tam:
 		gusano.add()
-		dibujar(gusano.cuerpo[gusano.cabeza])
+		dibujar(gusano.cuerpo[gusano.cabeza],verde_snake)
 	sincronizar = 600000 	
 	comida = Comida(ancho,alto,radio)
 	comida.add()
-	dibujar(comida.posicion)
+	dibujar(comida.posicion,naranja)
 	#Bloque principal
 	while True: 	
 		clock.tick(60)
@@ -55,8 +54,7 @@ def main():
 				pygame.quit()
 				sys.exit()
 		if teclado[K_ESCAPE]:
-			pygame.quit()
-			sys.exit()
+			return True
 		if teclado[K_UP]:
 			if (gusano.direccion != 90):
 				gusano.direccion = 270
@@ -79,18 +77,45 @@ def main():
 			a = 1	
 		borrar(gusano.cuerpo[gusano.cola])
 		gusano.avanzar()
-		dibujar(gusano.cuerpo[gusano.cabeza])
+		dibujar(gusano.cuerpo[gusano.cabeza],verde_snake)
 		comida.comio(gusano)
 		if comida.comido is True:
 			borrar(comida.posicion)
 			gusano.add()
-			dibujar(gusano.cuerpo[gusano.cabeza])
+			dibujar(gusano.cuerpo[gusano.cabeza],verde_snake)
 			comida.add()
-			dibujar(comida.posicion)
+			dibujar(comida.posicion,naranja)
 		pygame.display.flip()
 		pygame.display.update()
 
+def salir():
+	pygame.quit()
+	sys.exit()
+
+def instrucciones():
+	print('[K_UP] : Mover arriba \n [K_DOWN] : Mover abajo \n [K_LEFT] : Mover izquierda \n [K_RIGHT] : Mover Derecha \n [K_ESCAPE] : Volver al Menu')	
+	return True
+
 #PRINCIPAl
 if __name__ == '__main__':
-  	pygame.init()
-  	main()
+	opciones = [
+		("Jugar",snake),
+		("instrucciones",instrucciones),
+		("Salir",salir),
+		]
+	pygame.init()
+	pantalla = pygame.display.set_mode((ancho,alto))
+	pygame.display.set_caption('SNAKE')
+	fondo_menu = pygame.display.get_surface()
+	fondo_menu = fondo_menu.convert()
+	menu = Menu(opciones,pantalla,fondo_menu,ancho,alto)
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT :
+				pygame.quit()
+				sys.exit()
+		menu.screen.blit(menu.fondo,(0,0))
+		menu.imprimir()
+		menu.actualizar()
+		pygame.display.flip()
+		pygame.time.delay(20)
